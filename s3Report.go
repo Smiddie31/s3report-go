1ff2dbbc673602cc4278c703c46c33f401e2a579
Hayden Smith <hayden.smith@ofx.com>
2022-07-19 19:09:53 +1000

    Updated Script

--- s3Report.go
+++ s3Report.go
@@ -4,22 +4,38 @@ import (
 	"context"
 	"encoding/csv"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"log"
 	"os"
 	"strconv"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type s3Bucket struct {
 	name       string
-	versioning bool
+	versioning string
 	logging    bool
+}
+
+func getVersioning(n string, c *s3.Client) (v string) {
+	ver, err := c.GetBucketVersioning(context.TODO(), &s3.GetBucketVersioningInput{Bucket: &n})
+	if err != nil {
+		log.Fatalf("failed to get bucket versioning status, %v", err)
+	}
+	switch ver.Status {
+	case "Enabled":
+		return "Enabled"
+	case "Suspended":
+		return "Suspended"
+	default:
+		return "Not Enabled"
+	}
 }
 
 func main() {
 	var bucketData []*s3Bucket
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-2"))
 	if err != nil {
 		log.Fatalf("failed to load configuration, %v", err)
 	}
@@ -31,7 +47,8 @@ func main() {
 	}
 
 	for _, bucket := range buckets.Buckets {
-		bucketData = append(bucketData, &s3Bucket{*bucket.Name, true, false})
+		vStatus := getVersioning(*bucket.Name, s3Client)
+		bucketData = append(bucketData, &s3Bucket{*bucket.Name, vStatus, false})
 	}
 	file, err := os.Create("bucketdata.csv")
 	defer func(file *os.File) {
@@ -49,7 +66,7 @@ func main() {
 	var data [][]string
 	data = append(data, []string{"Name", "Versioning", "Logging"})
 	for _, record := range bucketData {
-		row := []string{record.name, strconv.FormatBool(record.versioning), strconv.FormatBool(record.logging)}
+		row := []string{record.name, record.versioning, strconv.FormatBool(record.logging)}
 		data = append(data, row)
 	}
 	errData := w.WriteAll(data)
